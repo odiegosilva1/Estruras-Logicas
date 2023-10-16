0b9511ddd66b8492b90358a7653593aaaae3209f
Diego Silva <odiegosilvadev@gmail.com>
2023-10-16 10:50:10 -0300

    update exe03

---
+++ exe03/index.js
@@ -0,0 +1,10 @@
+// Declara Variável
+let num = Number(prompt("Digite um número"));
+
+if (num <0) {
+    document.write(`${num} é negativo.`);
+} else if(num == 0) {
+    document.write(`O número ${num} é igual a 0.)`);
+} else {
+    document.write(`O número ${num} é positivo`);
+};   
